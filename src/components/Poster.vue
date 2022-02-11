08974b1750fa890b4ad30cddaa364a1d932d1c82
99ru <yan.99ru@gmail.com>
2022-02-11 15:34:49 +0100

    setup

---
+++ src/components/Poster.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <img :src="require('../assets/char-'+poster.id+'.png')" alt="poster image" height="100px">
+      <p>{{poster.job}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import posters from '@/posters.json'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    posters: [...posters],
   },
   mutations: {
   },
